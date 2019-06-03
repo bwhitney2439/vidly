c6161d9928a0ffe72a67c183283b58e7f2be1fc8
Whitney <benjamin.whitney@linklaters.com>
2019-06-03 16:41:25 +0100

    sgdf

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 
 
 class LoginForm extends Component {
+state = {
+    account: { username: "", password: "" }
+};
+
+    handleSubmit = e => {
+    e.preventDefault();
+    
+    // call the server 
+    const username = this.username.current.value
+    console.log('submitted');
+};
+
+handleChange = e => {
+    const account = {...this.state.account};
+    account.username = e.currentTarget.value;
+
+    this.setState({account});
+}
     render() { 
-        return <h1>Login</h1>
+        return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group"><label htmlFor="username"></label>Username<input value={this.state.account.username} onChange={this.handleChange} id="username" type="text" className="form-control"/></div>
+                <div className="form-group"><label htmlFor="password"></label>Password<input id="password" type="text" className="form-control"/></div>
+                <button className="btn btn-primary">Login</button>
+            </form>
+        </div>
+        )
     }
 }
  
